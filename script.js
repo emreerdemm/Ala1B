9753f362eef8fbf87dd7e46bdd94d44d75af8779
Emre <snlktl_419@icloud.com>
2022-10-06 22:25:46 +0200

    loadded

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// <script>
+
+// const toggleButton = document.getElementsByClassName('tbutton')[0]
+// const navbarLinks = document.getElementsByClassName('navbarlinks')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbarLinks.classList.toggle('active')
+// })
+//    </script>
